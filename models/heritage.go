8f1cc682a23684a79dbb084e94824128bc34e95d
skeletaljazzwizard <alexanderbrhoda@gmail.com>
2025-10-16 23:29:51 -0400

    WIP parsing heritages

--- models/heritage.go
+++ models/heritage.go
@@ -2,4 +2,19 @@ package models
 
 type heritage struct {
 	Name string
+	System heritageSystem
 }
+
+type heritageSystem struct {
+	commonSystem
+}
+
+func (h heritage) IsLegacy() bool {
+	return !h.System.Publication.Remaster
+}
+
+func (h heritage) HasProvidedLicense(license string) bool {
+	return h.System.Publication.License == license
+}
+
+
