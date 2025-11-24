308a2ce0c45ec5defe257fb769e6f1adc7dc0e11
Alexander <alexanderbrhoda@gmail.com>
2025-11-24 00:27:31 -0500

    add postgres and pgadmin to a local compose file. seed db with ./sql/pf2e.sql

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ PACKAGE ?= ./...
 CLI_PACKAGE ?= ./cmd/cli/main.go
 EXECUTABLE_NAME ?= tdm
 OUT_DIR ?= ./out
+PODMAN_DIR ?= ./podman
 
 .PHONY: build-cli
 build-cli: ## builds the cli binary 
@@ -25,6 +26,13 @@ vet: ## runs a `go vet` check for the project
 clean: ## remove `./out/` and all files in it
 	@rm -rf $(OUT_DIR)
 
+.PHONY: start-db
+start-db: ## starts the docker container for postgres and pgadmin using the compose file defined in the `./docker/ dir
+	podman compose -f $(PODMAN_DIR)/local-compose.yaml up --detach --build
+
+.PHONY: stop-db
+stop-db: ## stops the docker containers for postgres and pgadmin
+	podman compose -f $(PODMAN_DIR)/local-compose.yaml down --volumes
 
 .PHONY: help
 help: ## print this help message
--- sql/pf2e.sql
+++ sql/pf2e.sql
@@ -1,23 +1,23 @@
 CREATE TYPE licenses AS ENUM('OGL', 'ORC');
 CREATE TYPE rarities AS ENUM('common', 'uncommon', 'rare', 'unique');
-CREATE TYPE attributes AS ENUM('any', 'strength', 'dexterity', 'consitution', 'intelligence', 'wisdom', 'charisma');
+CREATE TYPE attributes AS ENUM('any', 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma');
 CREATE TYPE sizes AS ENUM('small', 'medium', 'large', 'huge', 'gargantuan');
 CREATE TYPE visions AS ENUM('normal', 'low light vision', 'dark vision');
 CREATE TYPE boost_types AS ENUM('first', 'second', 'third', 'flaw');
 
-CREATE TABLE trait IF NOT EXISTS (
-  trait_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  value text UNIQUE NOT NULL,
-  -- TODO ADD COLUMN description NOT NULL,
+CREATE TABLE IF NOT EXISTS trait (
+  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  value text UNIQUE NOT NULL
+  -- TODO ADD COLUMN description NOT NULL
 );
 
-CREATE TABLE language IF NOT EXISTS (
-  language_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  value text UNIQUE NOT NULL,
+CREATE TABLE IF NOT EXISTS language (
+  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  value text UNIQUE NOT NULL
 );
 
-CREATE TABLE sense IF NOT EXISTS (
-  sense_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE IF NOT EXISTS sense (
+  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name text NOT NULL, -- not unique!
   acuity int,
   range int,
@@ -28,9 +28,9 @@ CREATE TABLE sense IF NOT EXISTS (
     (name != 'low light vision' AND elevate_if_has_low_light_vision IS NULL))
 );
 
-CREATE TABLE boost IF NOT EXISTS (
-  boost_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY
-  attribute attributes NOT NULL,
+CREATE TABLE IF NOT EXISTS boost (
+  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  attribute attributes NOT NULL
 );
 
 -- CREATE TABLE proficiency IF NOT EXISTS (
@@ -40,8 +40,8 @@ CREATE TABLE boost IF NOT EXISTS (
 --   value int CHECK (0 <= value AND value <= 4)
 -- );
 
-CREATE TABLE ancestry IF NOT EXISTS (
-  ancestry_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE IF NOT EXISTS ancestry (
+  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name text UNIQUE NOT NULL,
   description text NOT NULL,
   game_master_description text,
@@ -58,36 +58,37 @@ CREATE TABLE ancestry IF NOT EXISTS (
   speed int NOT NULL,
   vision visions NOT NULL,
 
-  CONSTRAINT validate_hp_in_range check (6 <= hp AND hp <= 12 AND hp % 2 == 0)
-  CONSTRAINT validate_flaw_if_second_boost_present CHECK ((second_boost IS NOT NULL AND flaw IS NOT NULL) OR (second_boost IS NULL AND flaw IS NULL))
+  CONSTRAINT validate_hp_in_range check (6 <= hp AND hp <= 12 AND hp % 2 = 0)
+  -- TODO add this constraint to ancestries_boosts to require a flaw if a third boost for the ancestry_id is present
+  -- CONSTRAINT validate_flaw_if_second_boost_present CHECK ((second_boost IS NOT NULL AND flaw IS NOT NULL) OR (second_boost IS NULL AND flaw IS NULL))
 );
 
 -- junction table for this one to many mapping because boosts will also map to othe r tables
-CREATE TABLE ancestries_boosts IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS ancestries_boosts (
   ancestry_id int REFERENCES ancestry ON DELETE CASCADE,
   boost_id int REFERENCES boost ON DELETE CASCADE,
   boost_type boost_types NOT NULL,
-  PRIMARY KEY (ancestry_id, boost_id),
+  PRIMARY KEY (ancestry_id, boost_id)
 );
 
 -- junction table for this one to many mapping because traits will also map to othe r tables
-CREATE TABLE ancestries_traits IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS ancestries_traits (
   ancestry_id int REFERENCES ancestry ON DELETE CASCADE,
   trait_id int REFERENCES trait ON DELETE CASCADE,
-  PRIMARY KEY (ancestry_id, trait_id),
+  PRIMARY KEY (ancestry_id, trait_id)
 );
 
 -- junction table for this one to many mapping because languages will also map to othe r tables
-CREATE TABLE ancestries_languages IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS ancestries_languages (
   ancestry_id int REFERENCES ancestry ON DELETE CASCADE,
   language_id int REFERENCES language ON DELETE CASCADE,
   is_additional_language boolean NOT NULL,
-  PRIMARY KEY (ancestry_id, language_id),
+  PRIMARY KEY (ancestry_id, language_id)
 );
 
-CREATE TABLE ancestry_feature IF NOT EXISTS (
-  ancestry_feature_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  ancestry_id int REFERNECES ancestry ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS ancestry_feature (
+  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  ancestry_id int REFERENCES ancestry ON DELETE CASCADE,
   name text UNIQUE NOT NULL,
   description text NOT NULL,
   game_master_description text,
@@ -106,31 +107,31 @@ CREATE TABLE ancestry_feature IF NOT EXISTS (
   level int NOT NULL, -- might always be 0
   grants_languages_count int DEFAULT 0,
 
-  CONSTRAINT validate_grants_lang_count_if_present check ( grants_language_count >= 0)
+  CONSTRAINT validate_grants_lang_count_if_present check ( grants_languages_count >= 0)
 );
 
 -- junction table for this one to many mapping because languages will also map to othe r tables
-CREATE TABLE ancestry_features_languages IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS ancestry_features_languages (
   ancestry_features_id int REFERENCES ancestry_feature ON DELETE CASCADE,
   language_id int REFERENCES language ON DELETE CASCADE,
-  PRIMARY KEY (ancestry_features_id, language_id),
+  PRIMARY KEY (ancestry_features_id, language_id)
 );
 
-CREATE TABLE ancestry_features_traits IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS ancestry_features_traits (
   ancestry_feature_id int REFERENCES ancestry_feature ON DELETE CASCADE,
   trait_id int REFERENCES trait ON DELETE CASCADE,
-  PRIMARY KEY (ancestry_feature_id, trait_id),
+  PRIMARY KEY (ancestry_feature_id, trait_id)
 );
 
 -- junction table for this one to many mapping because senses will also map to othe r tables
-CREATE TABLE ancestry_features_senses IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS ancestry_features_senses (
   ancestry_feature_id int REFERENCES ancestry_feature ON DELETE CASCADE,
   sense_id int REFERENCES sense ON DELETE CASCADE,
-  PRIMARY KEY (ancestry_feature_id, sense_id),
+  PRIMARY KEY (ancestry_feature_id, sense_id)
 );
 
-CREATE TABLE background IF NOT EXISTS (
-  background_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE IF NOT EXISTS background (
+  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name text UNIQUE NOT NULL,
   description text NOT NULL,
   game_master_description text,
@@ -138,19 +139,19 @@ CREATE TABLE background IF NOT EXISTS (
   remaster boolean NOT NULL,
   license licenses NOT NULL,
   rarity rarities NOT NULL,
-  rules text,
+  rules text
 );
 
-CREATE TABLE skill IF NOT EXISTS (
-  skill_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name text UNIQUE NOT NULL,
+CREATE TABLE IF NOT EXISTS skill (
+  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name text UNIQUE NOT NULL
 );
 
 -- junction table for this one one to many mapping because skills will also map to othe r tables
-CREATE TABLE backgrounds_skills IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS backgrounds_skills (
   background_id int REFERENCES background ON DELETE CASCADE,
   skill_id int REFERENCES skill ON DELETE CASCADE,
-  PRIMARY KEY (background_id, skill_id),
+  PRIMARY KEY (background_id, skill_id)
 );
 
 -- create feats
